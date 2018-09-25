e7d904d8e1210fa6926684b5da6e10d045b2e6e8
StephenVarela <varelast92@gmail.com>
2018-09-25 15:26:48 -0400

    added navigation links functionality includes view individual pictures, show all pictures and adding new pictures form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,4 +6,24 @@ class PicturesController < ApplicationController
   def show
     @picture = Picture.find(params[:id])
   end
+
+  def create
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "/pictures"
+    else
+      render :new
+    end
+
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
 end
