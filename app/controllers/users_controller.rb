f4a5878bef1cea11737e6bbdb1761fd638583366
StephenVarela <varelast92@gmail.com>
2018-10-04 15:34:29 -0400

    added and tested user creation with flash error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(email: params[:user][:email],
+                    password: params[:user][:password],
+                    password_confirmation: params[:user][:password_confirmation])
+
+    if @user && @user.save
+      flash[:message] = ["user creation successful"]
+      redirect_to root_path
+    else
+      flash[:message] = @user.errors.full_messages
+      render :new
+    end
   end
 end
