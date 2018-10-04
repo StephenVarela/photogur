7be0d5d11ad0c399b0385ff66dfd31586786883a
StephenVarela <varelast92@gmail.com>
2018-10-04 16:15:43 -0400

    added resources routes to implement restful routes for pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
-  get 'pictures' => 'pictures#index'
-  post 'pictures' => 'pictures#create'
-  get  'pictures/new' => 'pictures#new'
   get 'pictures/old_pictures' => "pictures#old_pictures", as: :old_pictures
   get 'pictures/created_in_year/:id' => "pictures#created_in_year"
-  get 'pictures/:id' => 'pictures#show'
-  patch 'pictures/:id' => "pictures#update"
-  delete 'pictures/:id' => 'pictures#destroy'
 
-  get 'pictures/:id/edit' => "pictures#edit"
-
+  resources :pictures
   resources :users, only: [:new, :create]
   resources :sessions, only: [:index, :new, :create, :destroy]
 
